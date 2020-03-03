da324476e6c5bd1945354a4d8439b9cc4e6c5927
Deividas Brazenas <deividas.brazenas@gmail.com>
2020-03-03 16:37:17 +0200

    Updating persistence

---
+++ src/main/java/com/mif/backend/persistence/SponsorsDAO.java
@@ -0,0 +1,28 @@
+package com.mif.backend.persistence;
+
+import com.mif.backend.entities.Sponsor;
+import lombok.Setter;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@ApplicationScoped
+public class SponsorsDAO {
+    @PersistenceContext
+    @Setter
+    private EntityManager em;
+
+    public List<Sponsor> loadAll(){
+        return em.createNamedQuery("Sponsor.findAll", Sponsor.class).getResultList();
+    }
+
+    public void persist(Sponsor sponsor){
+        this.em.persist(sponsor);
+    }
+
+    public Sponsor findOne(Integer id) {
+        return em.find(Sponsor.class, id);
+    }
+}
